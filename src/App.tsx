f4b0de342419cc41cc51065a25a5132d933e8564
oku3san <takaya0303@gmail.com>
2020-10-27 23:04:08 +0900

    Fix issue of eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 function App() {
-  return (
-    <h1>
-      Hello, world!
-    </h1>
-  );
+  const greeting = 'Hi, Tom!';
+  const dom = <h1 className="foo">{greeting}</h1>;
+
+  return dom;
 }
 
 export default App;
