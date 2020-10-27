7d554b6e7849303bfd91a6d40c16cb84f2f24dcd
oku3san <takaya0303@gmail.com>
2020-10-27 23:47:29 +0900

    Create function components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
 import React from 'react';
 
+// function App() {
+//   return (
+//     <>
+//       <label htmlFor="bar">
+//         bar
+//         <input
+//           type="text"
+//           onChange={() => {
+//             console.log('I am clicked');
+//           }}
+//         />
+//       </label>
+//     </>
+//   );
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </div>
+//   );
+// };
+//
+// const Cat = () => {
+//   return <div>Meow!</div>;
+// };
+
 function App() {
   return (
-    <>
-      <label htmlFor="bar">
-        bar
-        <input
-          type="text"
-          onChange={() => {
-            console.log('I am clicked');
-          }}
-        />
-      </label>
-    </>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   );
+}
+
+function Cat() {
+  return <div>Meow!</div>;
 }
 
 export default App;
