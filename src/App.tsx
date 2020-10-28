30302d8b0599cced0e7f1f31cf7d5a66ebd551a5
oku3san <takaya0303@gmail.com>
2020-10-28 23:55:48 +0900

    Use map

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    { id: 1, name: 'Taro', age: 10 },
+    { id: 2, name: 'Taka', age: 31 },
+  ];
+
   return (
     <div>
-      <User name="Taro" />
+      {profiles.map((profile) => {
+        return <User name={profile.name} age={profile.age} key={profile.id} />;
+      })}
     </div>
   );
 }
 
 type Props = {
   name: string;
+  age: number;
 };
 
 const User: React.FC<Props> = (props) => {
-  const { name } = props;
+  const { name, age } = props;
 
   return (
     <>
-      <p>{name}!</p>
+      <p>
+        Hi, I am {name}! and {age} years old.
+      </p>
     </>
   );
 };
